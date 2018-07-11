4c4c9c9dc4cc838a2c9de5ac6e178441ac75d5dd
Jennydandancai <35747859+Jennydandancai@users.noreply.github.com>
2018-07-10 19:23:13 -0700

    Update app.py

--- website/app.py
+++ website/app.py
@@ -16,41 +16,38 @@ mysql.init_app(app)
 def home():
     return render_template("Prototype.html")
 
+def image_base64(item):
+        image_64 = base64.encodestring(item)
+        return image_64
+
+@app.route('/')
+def home():
+        return render_template("Prototype.html")
+
+
 @app.route('/Search', methods=['POST', 'GET'])
-def search():
-    try:
-        _search = request.form['search']
-
-    # validate the received values
-    if _search:
-
-
-    #All Good, let's call MySQL
-    ########################   BACK END TEAM   ######################################
-    #If everything has gone right, the _search variable above should be holding the search value
-
-    #Code from before, can use to help you get started
-    #         conn = mysql.connect()
-    #         cursor = conn.cursor()
-    #         _hashed_password = generate_password_hash(_password)
-    #         cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
-    #         data = cursor.fetchall()
-    #
-    #         if len(data) is 0:
-    #             conn.commit()
-    #             return json.dumps({'message': 'User created successfully !'})
-    #         else:
-    #             return json.dumps({'error': str(data[0])})
-    #     else:
-    #         return json.dumps({'html': '<span>Enter the required fields</span>'})
-    #
-    # except Exception as e:
-    #     return json.dumps({'error': str(e)})
-    # finally:
-    #     cursor.close()
-    #     conn.close()
-
-
+def searchResult():
+        error =''
+        try:
+                #flash("Come to try block")
+                if request.method == 'POST':
+                        conn = mysql.connect()
+                        cursor = conn.cursor()
+                        _search = request.form['search']
+                        order = "SELECT FullPic FROM Image WHERE ImageName Like '%%s%'"
+                        cursor.execute(order, (_search))
+                        conn.commit()
+                        data=cursor.fetchall()
+                        #flash(data)
+                        if(len(data) == 0):
+                                return redirect(url_for('/'))
+                        else:
+                                return render_template("PrototypeResult.html")
+                        cursor.close()
+                        conn.close()
+        except Exception as e:
+                #flash (e)
+                return render_template("Prototype.html",error = error)
 @app.route('/About')
 def about():
     return render_template("About.html")
