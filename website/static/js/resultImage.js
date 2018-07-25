13822c6062a5dab63d0c7409c5c9183f86ec1f51
rtelles64 <rtelles64@gmail.com>
2018-07-25 00:08:02 -0700

    Add imageResult.js

---
+++ website/static/js/resultImage.js
@@ -0,0 +1,10 @@
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Four images side by side
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";  // IE10
+        elements[i].style.flex = "25%";
+    }
+}
