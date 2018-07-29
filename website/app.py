9b70b830593d18f0e611cca541fba2612a97550e
25zsams <25zsams@gmail.com>
2018-07-28 17:37:15 -0700

    try again

--- website/app.py
+++ website/app.py
@@ -129,7 +129,7 @@ def adminPage():
         arg = [['aa', '111someone@gmail.com', '1/1/1/', 'azs', 'male', 'sj'], ['b', 'b111someone@gmail.com', '21/1/1/', 'bazs', 'fmale', 'nsj']]
     arg2 = [['/static/Images/IMG_20171113_140535.jpg', '25zs'], ['/static/Images/IMG_20171113_140535.jpg', 'azs']]
     arg3 = [['/static/Images/IMG_20171113_140535.jpg', '25zs'], ['/static/Images/IMG_20171113_140535.jpg', 'azs']]
-    return render_template("About/AdminPage.html", userData = arg, pendingData = arg2, approvedData = arg3)
+    return render_template("/AdminPage.html", userData = arg, pendingData = arg2, approvedData = arg3)
 
 @app.route('/About')
 def about():
