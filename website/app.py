fea0d5c2a235ac322e1bdb422e778351df0819c6
Patrick Coico <PCoico219@yahoo.com>
2018-08-02 16:39:32 -0700

    Patrick Debug 2

--- website/app.py
+++ website/app.py
@@ -110,8 +110,8 @@ def imagePage(image):
     views = cursor.fetchall()
    
     flash(views[0][0])    
-    views[0][0] = views[0][0] + 1
-    flash(views[0][0])
+    #views[0][0] = views[0][0] + 1
+    #flash(views[0][0])
     #view = "Update ApprovedImg set views=%s + 1 where ImageId=(?)"
     #cursor.execute(view, data[0][0])
     #conn.commit()
