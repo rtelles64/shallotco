208619d77a6ac299c1aa9d5f59f777bd3d148d80
Patrick Coico <PCoico219@yahoo.com>
2018-08-07 21:39:09 -0700

    Change single quotes to double

--- website/app.py
+++ website/app.py
@@ -165,7 +165,7 @@ def uploadImage():
             for file in request.files.getlist("file"):
                 filename = file.filename
                 if filename == '':
-                    error_ = 'You must choose an image to upload'
+                    error_ = "You must choose an image to upload"
                     return render_template("UploadImage.html",error=error_)
                 else:
                     #create destination to save the file
